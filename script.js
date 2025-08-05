06f400d06239565eef43fead15ec64c7ad4d88c1
dhruvaggarwal001 <dhruvaggarwal001@gmail.com>
2025-08-05 15:51:25 +0530

    Final

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const inputBox = document.getElementById("input-box")
 const lists = document.getElementById("todolist")
 
 const fetchTodos = async () => {
-    const response = await fetch ("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
     const todos = await response.json()
-    lists.innerHTML = todos.map((todo) => {
-        let li = document.createElement("li")
-        li.innerText = todo.title
-        return li
-    })
+    lists.innerHTML = todos.map(todo => `<li class="myTodos">${todo.title}</li>`).join("");
 }
+fetchTodos()
